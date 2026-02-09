1a0bf0c4d411ce833028597709bc752e9f92ac06
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-02-09 13:22:45 -0800

    Installed mathjax and using as default latex rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 
 import './App.css'
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+
+import MarkDownEditor from './components/MarkdownEditor/MarkdownEditor'
+
+const mathJaxConfig = {
+  tex: {
+    inlineMath: [["$", "$"]],
+    displayMath: [["$$", "$$"]],
+  },
+};
+
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-blue-500 text-3xl'>Hello World</h1></>
+
+    <MathJaxContext config={mathJaxConfig}>
+      <h1 className='text-blue-500 text-3xl'>MarkdownEditor</h1>
+      <MarkDownEditor />
+    </MathJaxContext>
+
   )
 }
 
---
+++ frontend/src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+import { MathJax } from "better-react-mathjax";
+
+export default function MarkDownEditor() {
+  const [value, setValue] = React.useState<string | undefined>("");
+
+  return (
+    <MathJax>
+      <MDEditor value={value} onChange={(val) => setValue(val)} />
+    </MathJax>
+  );
+}
