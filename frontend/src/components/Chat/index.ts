55ca218bff73a2a0b4ce835960a686b45aa809bb
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-03-02 11:28:39 -0800

    Fixed changes

--- frontend/src/components/Chat/index.ts
+++ frontend/src/components/Chat/index.ts
@@ -1,4 +1,5 @@
 export { default as ChatMessage } from "./ChatMessage";
 export { default as ChatContainer } from "./ChatContainer";
 export { default as ChatInput } from "./ChatInput";
-export {default as SourceSection} from "./Sources"
+export { default as SourceSection}  from "./Sources";
+export { default as ChatSideBar } from "./ChatSideBar";
