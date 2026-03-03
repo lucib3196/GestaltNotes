b719e65a9a80d7caadb4d1d4c7bac8fb40991b52
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-03-03 10:59:33 -0800

    fixed imports

--- frontend/src/features/Chat/index.ts
+++ frontend/src/features/Chat/index.ts
@@ -1,5 +1,5 @@
-export { default as ChatMessage } from "./ChatMessage";
-export { default as ChatContainer } from "./ChatContainer";
-export { default as ChatInput } from "./ChatInput";
-export { default as SourceSection}  from "./Sources";
-export { default as ChatSideBar } from "./ChatSideBar";
+export { default as ChatMessage } from "./components/ChatMessage";
+export { default as ChatContainer } from "./components/ChatContainer";
+export { default as ChatInput } from "./components/./ChatInput";
+export { default as SourceSection } from "./components/./Sources";
+export { default as ChatSideBar } from "./components/./ChatSideBar";
