da8fd18cb7819684d9eaefa6060e9562b42769a5
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-03-05 14:47:11 -0800

    ensured users must be logged in to use

--- frontend/src/context/ChatContext.tsx
+++ frontend/src/context/ChatContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext, } from "react";
 import type { LectureMetadata } from "../types/Metadata.types";
 
 
-export type ValidAgent = "agent_me135" | "agent_me118";
+export type ValidAgent =  "agent_me118";
 
 export type LectureArtifact = {
     id: string;
@@ -26,7 +26,7 @@ const LectureChatContext = createContext<LectureChatContext | undefined>(undefin
 export function LectureChatProvider({ children }: { children: React.ReactNode }) {
     const [sources, setSources] = useState<LectureArtifact[]>([])
     const [threadId, setThreadId] = useState<string | null>(null);
-    const [agent, setAgent] = useState<ValidAgent>("agent_me135")
+    const [agent, setAgent] = useState<ValidAgent>("agent_me118")
 
     return <LectureChatContext.Provider value={{ sources, setSources, agent, setAgent, threadId, setThreadId }}>
         {children}
