7aa2a5589c04914d071ea77081e77dadf23f7dfe
Luciano Bermudez <lberm007@ucr.edu>
2026-05-25 12:58:39 -0700

    Added init file for proper imports

---
+++ backend/src/model/__init__.py
@@ -0,0 +1,15 @@
+from .chat import Thread, Message
+from .course import Course, StudentCourseLink, LectureNote
+from .user import UserRoleLink, UserCourseLink, User, Role
+
+__all__ = [
+    "Thread",
+    "Message",
+    "Course",
+    "StudentCourseLink",
+    "LectureNote",
+    "UserRoleLink",
+    "UserCourseLink",
+    "User",
+    "Role",
+]
